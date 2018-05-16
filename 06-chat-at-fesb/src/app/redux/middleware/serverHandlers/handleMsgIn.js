28f64338a85e5862fd82dbfc1d22f825758d86d3
Rato Kuzmanić <rato.kuzmanic@live.com>
2018-05-16 15:18:22 +0200

    Add auth tag checking and decryption

--- 06-chat-at-fesb/src/app/redux/middleware/serverHandlers/handleMsgIn.js
+++ 06-chat-at-fesb/src/app/redux/middleware/serverHandlers/handleMsgIn.js
@@ -1,26 +1,29 @@
+const crypto = require('crypto')
+
 import { serverMsg } from 'app/redux/actions/serverActions.js'
 import { JSONparse } from 'app/utils/safeJSON.js'
 import { clientError } from 'app/redux/actions/clientActions.js'
 import { loadKey } from './utils.js'
+import CryptoProvider from '../../../services/security/CryptoProvider.js'
 
 export default ({ getState, dispatch }, next, action) => {
     const { meta: { serialized } } = action
     if (!serialized) return next(action)
 
-    let msg = JSONparse(action.payload)
+    let message = JSONparse(action.payload)
 
-    if (Object.is(msg, undefined)) {
+    if (Object.is(message, undefined)) {
         return dispatch(clientError(`JSON.parse error: ${data}`))
     }
 
-    if (msg.id) {
+    if (message.id) {
         const { credentials } = getState()
 
         //=================================================== 
         // Try to load an encryption key for this client id;
         // please note that this is a remote client.
         //===================================================         
-        const key = loadKey(msg.id, credentials)
+        const key = loadKey(message.id, credentials)
 
         //=================================================== 
         // If the encryption key is successfully loaded,
@@ -29,10 +32,37 @@ export default ({ getState, dispatch }, next, action) => {
         // So, we decrypt the messages before reading them.  
         //===================================================
         if (key) {
-            msg = {...msg}
-            msg.content = `DECRYPTED(${msg.content})`
+            const decryptionKey = Buffer.alloc(32);
+            const hmacKey = Buffer.alloc(32);
+            key.copy(decryptionKey, 0, 0, key.byteLength / 2);
+            key.copy(hmacKey, 0, key.byteLength / 2, 64);
+
+            message = {...message};
+
+            const messageWithoutAuthTag = Object.keys(message)
+            .filter(key => key !== 'authTag')
+            .reduce((obj, key) => {
+                obj[key] = message[key];
+                return obj;
+            }, {});
+            const hmac = crypto.createHmac('sha256', hmacKey);
+            hmac.update(JSON.stringify(messageWithoutAuthTag));
+            const digest = hmac.digest().toString('hex');
+            const authTag = digest.slice(0, digest.length / 2);
+
+            if(authTag === message.authTag) {
+                const { plaintext } = CryptoProvider.decrypt('CBC', {
+                    key: decryptionKey,
+                    iv: Buffer.from(message.iv, 'hex'),
+                    ciphertext: message.content
+                });
+                message.content = plaintext;
+            }
+            else {
+                message.content = `AUTHENTICATION FAILURE`
+            }            
         }
     }
 
-    dispatch(serverMsg(msg))
+    dispatch(serverMsg(message))
 }
