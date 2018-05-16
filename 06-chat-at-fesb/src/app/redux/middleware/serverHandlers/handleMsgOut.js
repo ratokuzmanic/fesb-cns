5f17d02e779fb95ab4ea6564e829452ededcf6ac
Rato Kuzmanić <rato.kuzmanic@live.com>
2018-05-16 13:39:00 +0200

    Add encryption of output messages

--- 06-chat-at-fesb/src/app/redux/middleware/serverHandlers/handleMsgOut.js
+++ 06-chat-at-fesb/src/app/redux/middleware/serverHandlers/handleMsgOut.js
@@ -1,7 +1,11 @@
+const crypto = require('crypto')
+
 import { Server, Constants } from 'config'
 import serverAPI from 'app/services/server-api/ServerAPI.js'
 import { msgSent } from 'app/redux/actions/clientActions.js'
 import { loadKey } from './utils.js'
+import CryptoProvider from '../../../services/security/CryptoProvider.js'
+import { randomBytes } from 'crypto';
 
 const { MsgType } = Constants
 
@@ -24,15 +28,27 @@ export default ({ getState, dispatch }, next, action) => {
     // it is implied that all outgoing messages from this 
     // client will be encrypted with that key.  
     //===================================================
-    const msg = {
+  
+    const message = {
         type: MsgType.BROADCAST,
         id,
         nickname,
-        timestamp: Date.now(),
-        content: key ? `ENCRYPTED(${action.payload})` : action.payload
+        timestamp: Date.now()
     }
 
-    serverAPI.send(msg).then(
-        dispatch(msgSent(msg))
-    )    
+    if(key) {
+        const { ciphertext: content, iv } = CryptoProvider.encrypt('CBC', {
+            key,
+            iv: randomBytes(16),
+            plaintext: action.payload
+        })
+        Object.assign(message, { content, iv });         
+    }
+    else {
+        Object.assign(message, { content: action.payload });
+    }  
+
+    serverAPI.send(message).then(
+        dispatch(msgSent(message))
+    )
 }
