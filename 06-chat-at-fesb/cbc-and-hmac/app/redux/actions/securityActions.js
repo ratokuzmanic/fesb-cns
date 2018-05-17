45cf09894b39a6dd57b74aaaf50df4dfe315c82a
Rato Kuzmanić <rato.kuzmanic@live.com>
2018-05-17 20:34:43 +0200

    Remove comment code

--- 06-chat-at-fesb/cbc-and-hmac/app/redux/actions/securityActions.js
+++ 06-chat-at-fesb/cbc-and-hmac/app/redux/actions/securityActions.js
@@ -28,38 +28,6 @@ export const generateKey = payload => dispatch => {
             },
             error: true
         }))
-
-    //=================================================== 
-    // Replace the above with true PBKDF2 function. 
-    //
-    // If KEY generated successfully dispatch 
-    // the following message:
-    //
-    //    const msg = {
-    //         type: KEY_GENERATED,
-    //         payload: { 
-    //             id: payload.id, 
-    //             key: key // Generated key 
-    //         }
-    //    }
-    //
-    // The message can be dispatched as follows:
-    //
-    //     dispatch(msg)
-    //
-    // If the key generation process results in a failure 
-    // with error "error", disptach the following message:
-    //
-    //    const msg = {
-    //         type: KEY_GENERATE,
-    //         payload: { 
-    //             id: payload.id, 
-    //             error: error // Resulting error
-    //         }
-    //         error: true
-    //    }
-    //
-    //===================================================
 }
 
 export const deleteKey = id => ({
